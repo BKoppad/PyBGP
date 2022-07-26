86417febb651dc0f676dbfefe5938f789bd38a4f
BKoppad <basuk.ec04@gmail.com>
2022-07-26 17:40:53 +0100

    Updating test script

--- app/bgp_router_config.py
+++ app/bgp_router_config.py
@@ -1,43 +1,40 @@
-from paramiko import SSHClient, AutoAddPolicy
+import re
+from paramiko import AuthenticationException, BadHostKeyException, SSHClient, AutoAddPolicy, SSHException
 import time
 
-def connect_ssh(router):
-    '''Connecting to router and returning sshClient object'''
-    ssh_client = SSHClient()
-    ssh_client.set_missing_host_key_policy(AutoAddPolicy())
-    print(f'Connecting to {router["hostname"]}')
-    ssh_client.connect(**router, look_for_keys=False, allow_agent=False)
-    return ssh_client
+class BGP_Router:
+    def connect_ssh(self, router):
+        '''Connecting to router and returning sshClient object'''
+        self.ssh_client = SSHClient()
+        self.ssh_client.set_missing_host_key_policy(AutoAddPolicy())
+        print(f'Connecting to {router["hostname"]}')
+        try :
+            self.ssh_client.connect(**router, look_for_keys=False, allow_agent=False)
+        except Exception as sshExc:
+            print("ERROR: Unable to establish ssh connection: %s" % sshExc)
+            return False
 
-def cli_access(ssh_client):
-    '''Executing commands on supplied sshClient object or resepctive router'''
-    shell = ssh_client.invoke_shell()
-    print(shell.send_ready())
-    shell.send('enable\n')
-    shell.send('cisco\n')
-    time.sleep(1)
-    output = shell.recv(10000).decode()
-    print(output)
-    command = " "
-    
-    while command != 'exit':
-        command= input("i/p > ")
-        shell.send(f"{command}\n")
-        time.sleep(3)
-        output=shell.recv(10000).decode()
+        print("Connected successfully with router " , router["hostname"] )
+        return True
+
+    def cli_access(self):
+        '''Executing commands on supplied sshClient object or resepctive router'''
+        self.shell = self.ssh_client.invoke_shell()
+        print(self.shell.send_ready())
+        self.shell.send('enable\n')
+        self.shell.send('cisco\n')
+        time.sleep(1)
+        output = self.shell.recv(10000).decode()
         print(output)
-
-
-# creating a dictionary for each device to connect to
-router1 = {'hostname': '10.1.1.10', 'port': '22', 'username':'bkoppad', 'password':'cisco'}
-router2 = {'hostname': '10.1.1.20', 'port': '22', 'username':'bkoppad', 'password':'cisco'}
-
-routers = [router1,router2]
-
-for router in routers:
-    ssh_client = connect_ssh(router)
-    cli_access(ssh_client)
-
-    if ssh_client.get_transport().is_active() == True:
-        print('Closing connection')
-        ssh_client.close()
+        command = " "
+        
+        while command != 'exit':
+            command= input("> ")
+            self.shell.send(f"{command}\n")
+            time.sleep(3)
+            output=self.shell.recv(10000).decode()
+            print(output)
+    
+    def __del__(self):
+        print("Closing connection")
+        self.ssh_client.close()
